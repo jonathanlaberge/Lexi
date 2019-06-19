60d4d4a0e68475d28e10ee12a80715ecec78a3f9
Jonathan Laberge <pm@jolab.me>
2019-06-19 10:58:30 -0400

    API Connection

--- Lexi/src/app/service/api.service.ts
+++ Lexi/src/app/service/api.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Maitresse } from '../model/maitresse';
+import { Eleve } from '../model/eleve';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class APIService {
+export const API_URL = "http://lexi.jolab.me/v1/";
 
-  constructor() { }
+@Injectable(
+    {
+        providedIn: 'root'
+    })
+export class APIService
+{
+    private options = { headers: new HttpHeaders().set('Content-Type', 'application/json') };
+
+    public static currentMaitresse: Maitresse = null;
+    public static currentEleve: Eleve = null;
+    public static token: string = null;
+
+
+    constructor(private httpClient: HttpClient)
+    {
+
+    }
+
+    private RefreshHeader()
+    {
+        if (APIService.token != null)
+            this.options =
+                {
+                    headers: new HttpHeaders().set('Content-Type', 'application/json')
+                        .append('Authorization', `Bearer ${APIService.token}`)
+                        .append('Access-Control-Allow-Origin', '*')
+                };
+        else
+            this.options = { headers: new HttpHeaders().set('Content-Type', 'application/json') };
+    }
+
+
+
+    public Connection(maitresse: Maitresse)
+    {
+        this.RefreshHeader();
+        return this.httpClient.post(`${API_URL}compte/connection`, JSON.stringify(maitresse), this.options);
+    }
 }
