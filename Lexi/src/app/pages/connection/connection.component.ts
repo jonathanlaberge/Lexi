c8d95bba8d94571d70c6a91897f3493404988a06
Jonathan Laberge <pm@jolab.me>
2019-06-20 11:28:43 -0400

    Connection Completed

--- Lexi/src/app/pages/connection/connection.component.ts
+++ Lexi/src/app/pages/connection/connection.component.ts
@@ -1,21 +1,20 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl, Validators } from "@angular/forms";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { ClrForm } from '@clr/angular';
 import { Maitresse } from 'src/app/model/maitresse';
 import { APIService } from 'src/app/service/api.service';
-import { Router, ActivatedRoute, Data } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { ConnectionDTO } from 'src/app/model/dto/connection-dto';
 import { RoutingService } from 'src/app/service/routing.service';
 
-
-
-
-@Component({
-    selector: 'app-connection',
-    templateUrl: './connection.component.html',
-    styleUrls: ['./connection.component.css']
-})
-export class ConnectionComponent implements OnInit {
+@Component(
+    {
+        selector: 'app-connection',
+        templateUrl: './connection.component.html',
+        styleUrls: ['./connection.component.css']
+    })
+export class ConnectionComponent implements OnInit
+{
     @ViewChild(ClrForm, { static: true }) loginClrForm;
     @ViewChild(ClrForm, { static: true }) registerClrForm;
 
@@ -23,292 +22,191 @@ export class ConnectionComponent implements OnInit {
     loginForm: FormGroup;
     registerForm: FormGroup;
 
-
-
     return: string = '';
     isRegisterModalOpen: boolean = false;
 
-    erreurLogin: boolean = false;
-    erreurServeur: boolean = false;
-    erreurEmail: boolean = false;
-    erreurServeurModal: boolean = false;
+    errorLoginInvalid: boolean = false;
+    errorLoginServer: boolean = false;
+    successRegister: boolean = false;
+    errorRegisterExist: boolean = false;
+    errorRegisterServer: boolean = false;
 
+    constructor(
+        private formBuilder: FormBuilder,
+        private router: Router,
+        private route: ActivatedRoute,
+        private apiService: APIService) { }
 
+    ngOnInit()
+    {
+        this.loginForm = this.formBuilder.group(
+            {
+                email: ['', [Validators.required, Validators.email]],
+                motdepasse: ['', [Validators.required, Validators.minLength(8)]]
+            });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    constructor(private formBuilder: FormBuilder, private router: Router, private route: ActivatedRoute, private apiService: APIService) {
-
-    }
-
-
-
-
-    ngOnInit() {
-        this.loginForm = this.formBuilder.group({
-
-            email: ['', [Validators.required, Validators.email]],
-            motdepasse: ['', [Validators.required, Validators.minLength(8)]],
-
-        }//, {
-            //  validator: MustMatch('password', 'confirmPassword')
-            // }
-        );
-
-        this.registerForm = this.formBuilder.group({
-
-            email: ['', [Validators.required, Validators.email]],
-            motdepasse: ['', [Validators.required, Validators.minLength(8)]],
-            confirmationmotdepasse: ['', [Validators.required, Validators.minLength(8)]],
-            prenom: ['', [Validators.required]],
-            nom: ['', [Validators.required,]],
-            dateNaissance: ['', []],
-            genre: ['', []]
-
-        }, {
+        this.registerForm = this.formBuilder.group(
+            {
+                email: ['', [Validators.required, Validators.email]],
+                motdepasse: ['', [Validators.required, Validators.minLength(8)]],
+                confirmationmotdepasse: ['', [Validators.required, Validators.minLength(8)]],
+                prenom: ['', [Validators.required]],
+                nom: ['', [Validators.required,]],
+                dateNaissance: ['', []],
+                genre: ['', []]
+            },
+            {
                 validator: MustMatch('motdepasse', 'confirmationmotdepasse'),
-            }
-        );
+            });
 
-
-
-        /*localStorage.setItem('maitresseInfo', JSON.stringify(APIService.currentMaitresse));
-        localStorage.setItem('token', JSON.stringify(APIService.token));*/
-
-
-
-        if (localStorage.getItem('maitresseInfo') != null && localStorage.getItem('token') != null ) {
-
+        if (localStorage.getItem('maitresseInfo') != null && localStorage.getItem('token') != null)
+        {
             APIService.currentMaitresse = JSON.parse(localStorage.getItem('maitresseInfo')) as Maitresse;
-
 
             RoutingService.isLoggedIn = true;
             RoutingService.adminMode = true;
             APIService.token = JSON.parse(localStorage.getItem('token'));
 
-           RoutingService.SetRouteToAdmin();
-
+            RoutingService.SetRouteToAdmin
         }
 
-            
-
-
-
-
-
-
-
-        this.route.queryParams.subscribe(params => {
+        this.route.queryParams.subscribe(params =>
+        {
             this.return = params['return'] || '/tableaudebord';
             if (RoutingService.isLoggedIn == true)
                 this.router.navigateByUrl(this.return);
         });
-
-
-
-
-
     }
 
+    get errorMessage()
+    {
+        return this.loginForm.controls;
+    }
 
-    get errorMessage() { return this.loginForm.controls; }
+    submitLogin()
+    {
+        this.errorLoginInvalid = false;
+        this.errorLoginServer = false;
+        this.successRegister = false;
+        this.errorRegisterExist = false;
+        this.errorRegisterServer = false;
 
-
-
-
-
-    submit() {
-        if (this.loginForm.invalid) {
+        if (this.loginForm.invalid)
+        {
             this.loginClrForm.markAsTouched();
-            console.log("bad value");
+            this.errorLoginInvalid = true;
         }
-
-        else {
-
-            //this.loginForm.value.email;
-            //  this.loginForm.value.password;
-
-            console.log(this.loginForm.value);
-            console.log(this.loginForm.value.email);
-            console.log(this.loginForm.value.motdepasse);
-
-
-
-
-
-
-
-            var credential: ConnectionDTO;
-
-
-
-
-
+        else
+        {
             var maitresse: Maitresse = new Maitresse();
-            maitresse.motdepasse = this.loginForm.value.motdepasse as string;
-            maitresse.email = this.loginForm.value.email as string;
 
+            maitresse.motdepasse = this.loginForm.value.motdepasse;
+            maitresse.email = this.loginForm.value.email;
 
+            this.apiService.Connection(maitresse).subscribe(
+                (data: any) =>
+                {
+                    if (data.token != null)
+                    {
+                        var credential: ConnectionDTO;
+                        credential = (data as ConnectionDTO);
+                        
+                        APIService.currentMaitresse = (credential.account as Maitresse);
+                        
+                        RoutingService.SetRouteToAdmin();
+                        RoutingService.isLoggedIn = true;
+                        RoutingService.adminMode = true;
+                        APIService.token = credential.token;
+                        
+                        localStorage.setItem('maitresseInfo', JSON.stringify(APIService.currentMaitresse));
+                        localStorage.setItem('token', JSON.stringify(APIService.token));
 
-            this.apiService.Connection(maitresse).subscribe((data: any) => {
-                if (data != null) {
-                    // error credentiel
-                    APIService.currentMaitresse = null;
-
+                        this.router.navigateByUrl(this.return);
+                    }
+                    else
+                        this.errorLoginServer = true;
+                },
+                error =>
+                {
+                    if (error.status == 401)
+                        this.errorLoginInvalid = true;
+                    else
+                        this.errorLoginServer = true;
                 }
-
-                if(data.token) {
-                    credential = (data as ConnectionDTO);
-
-
-                    APIService.currentMaitresse = (credential.account as Maitresse);
-
-
-                    //CONTINUE IF LOGIN VALID
-                    RoutingService.SetRouteToAdmin();
-                    RoutingService.isLoggedIn = true;
-                    RoutingService.adminMode = true;
-                    APIService.token = credential.token;
-
-
-
-                    localStorage.setItem('maitresseInfo', JSON.stringify(APIService.currentMaitresse));
-                    localStorage.setItem('token', JSON.stringify(APIService.token));
-
-                    this.router.navigateByUrl(this.return);
-                }
-
-            
-            });
-
+            );
         }
-
-
     }
 
-
-
-
-
-
-
-
-
-
-    openRegisterModalForm() {
+    openRegisterModalForm()
+    {
         this.isRegisterModalOpen = true;
     }
 
+    registerModalForm()
+    {
+        this.errorLoginInvalid = false;
+        this.errorLoginServer = false;
+        this.successRegister = false;
+        this.errorRegisterExist = false;
+        this.errorRegisterServer = false;
 
-
-
-    registerModalForm() {
-
-
-
-        if (this.registerForm.invalid) {
+        if (this.registerForm.invalid)
+        {
             this.registerClrForm.markAsTouched();
-            console.log("bad value");
-
-        } else {
-
-            //this.loginForm.value.email;
-            //  this.loginForm.value.password;
-
-            console.log(this.registerForm.value);
-
-
-
+        }
+        else
+        {
             var maitresse: Maitresse = new Maitresse();
 
-
-            maitresse.motdepasse = this.registerForm.value.motdepasse as string;
-            maitresse.email = this.registerForm.value.email as string;
-            maitresse.prenom = this.registerForm.value.prenom as string;
-            maitresse.nom = this.registerForm.value.nom as string;
-
-           
+            maitresse.motdepasse = this.registerForm.value.motdepasse;
+            maitresse.email = this.registerForm.value.email;
+            maitresse.prenom = this.registerForm.value.prenom;
+            maitresse.nom = this.registerForm.value.nom;
             maitresse.dateNaissance = this.registerForm.value.dateNaissance as Date;
+            maitresse.genre = this.registerForm.value.genre;
 
-            maitresse.genre = this.registerForm.value.genre as number;
-
-
-
-            this.apiService.Enregistrement(maitresse).subscribe((data: any) => {
-
-
-
-
-
-
-
-
-
-
-
-                if (data.code == 401) {
-
-                    this.erreurEmail = false;
-                }
-                else if (data.code == 500)
+            this.apiService.Enregistrement(maitresse).subscribe(
+                (data: any) =>
                 {
-                    this.erreurServeurModal = false;
-                } else if (data.code == 400) {
-
-                    this.erreurEmail = false;
-                }
-
-            });
-
-
-
-            
-
-
-           // this.isRegisterModalOpen = !this.isRegisterModalOpen;
+                    if (data.code == 200)
+                    {
+                        this.successRegister = true;
+                        this.isRegisterModalOpen = false;
+                    }
+                    else
+                        this.errorRegisterServer = true;
+                },
+                error =>
+                {
+                    if (error.status == 401)
+                        this.errorRegisterExist = true;
+                    else
+                        this.errorRegisterServer = true;
+                });
         }
-    }
-
-
-
-
-
-
-
-
-    resetRegisterModalForm() {
-        this.registerForm.reset();
     }
 }
 
-
-export function MustMatch(controlName: string, matchingControlName: string) {
-    return (formGroup: FormGroup) => {
+export function MustMatch(controlName: string, matchingControlName: string)
+{
+    return (formGroup: FormGroup) =>
+    {
         const control = formGroup.controls[controlName];
         const matchingControl = formGroup.controls[matchingControlName];
 
-        if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+        if (matchingControl.errors && !matchingControl.errors.mustMatch)
+        {
             // return if another validator has already found an error on the matchingControl
             return;
         }
 
         // set error on matchingControl if validation fails
-        if (control.value !== matchingControl.value) {
+        if (control.value !== matchingControl.value)
+        {
             matchingControl.setErrors({ mustMatch: true });
-        } else {
+        }
+        else
+        {
             matchingControl.setErrors(null);
         }
 
