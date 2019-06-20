1b97af1ecb06a9545d6f7a3f3a665ec7f8cc1a89
Jonathan Laberge <pm@jolab.me>
2019-06-20 03:29:35 -0400

    Removing CurseurTriangulaire

--- Lexi/src/app/ui/curseurs/curseurs.module.ts
+++ Lexi/src/app/ui/curseurs/curseurs.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CurseurSimpleComponent } from './curseur-simple/curseur-simple.component';
 import { CurseurCirculaireComponent } from './curseur-circulaire/curseur-circulaire.component';
-import { CurseurTriangulaireComponent } from './curseur-triangulaire/curseur-triangulaire.component';
 import { CurseurImageComponent } from './curseur-image/curseur-image.component';
 
 @NgModule(
     {
-        declarations: [CurseurSimpleComponent, CurseurCirculaireComponent, CurseurTriangulaireComponent, CurseurImageComponent],
+        declarations: [CurseurSimpleComponent, CurseurCirculaireComponent, CurseurImageComponent],
         imports: [
             CommonModule
         ],
-        exports: [CurseurSimpleComponent, CurseurCirculaireComponent, CurseurTriangulaireComponent, CurseurImageComponent]
+        exports: [CurseurSimpleComponent, CurseurCirculaireComponent, CurseurImageComponent]
     })
 export class CurseursModule { }
