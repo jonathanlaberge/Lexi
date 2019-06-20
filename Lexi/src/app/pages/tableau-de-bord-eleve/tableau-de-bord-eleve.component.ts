ac7925f2b5a9701f1cad6dbd19ec2aba405f25f2
Jonathan Laberge <pm@jolab.me>
2019-06-20 09:00:22 -0400

    Update tableau-de-bord-eleve.component.ts

--- Lexi/src/app/pages/tableau-de-bord-eleve/tableau-de-bord-eleve.component.ts
+++ Lexi/src/app/pages/tableau-de-bord-eleve/tableau-de-bord-eleve.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { RoutingService } from 'src/app/service/routing.service';
 
-@Component({
-  selector: 'app-tableau-de-bord-eleve',
-  templateUrl: './tableau-de-bord-eleve.component.html',
-  styleUrls: ['./tableau-de-bord-eleve.component.css']
-})
-export class TableauDeBordEleveComponent implements OnInit {
+@Component(
+    {
+        selector: 'app-tableau-de-bord-eleve',
+        templateUrl: './tableau-de-bord-eleve.component.html',
+        styleUrls: ['./tableau-de-bord-eleve.component.css']
+    })
+export class TableauDeBordEleveComponent implements OnInit
+{
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit()
+    {
+        RoutingService.adminMode = false;
+        RoutingService.SetRouteToEleve();
+    }
 
 }
