83f973a2dd62aa43cb1af76923416d4d543bb177
Jonathan Laberge <pm@jolab.me>
2019-06-20 04:41:39 -0400

    Navbar progress

--- Lexi/src/app/ui/dashboard/navbar/navbar.component.ts
+++ Lexi/src/app/ui/dashboard/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { RoutingService } from 'src/app/service/routing.service';
+import { APIService } from 'src/app/service/api.service';
+import { Router } from '@angular/router';
 
 @Component(
     {
@@ -12,21 +14,41 @@ export class NavbarComponent implements OnInit
 {
     isAdmin = false;
     eleveConnected = false;
+    name = null;
 
     routeSubscription: Subscription;
 
-    constructor() { }
+    constructor(private router: Router) { }
 
     ngOnInit()
     {
         this.isAdmin = RoutingService.adminMode;
         this.eleveConnected = RoutingService.eleveConnected;
+        this.name = `${APIService.currentMaitresse.prenom} ${APIService.currentMaitresse.nom}`;
 
         this.routeSubscription = RoutingService.routeSubject.subscribe((/*route: any[]*/) => 
         {
             this.isAdmin = RoutingService.adminMode;
             this.eleveConnected = RoutingService.eleveConnected;
-        })
+            this.name = `${APIService.currentMaitresse.prenom} ${APIService.currentMaitresse.nom}`;
+        });
     }
 
+    ShowProfile()
+    {
+        ///////////////////////////////////////////////////////////////////////////////////////////////
+    }
+
+    Logout()
+    {
+        localStorage.clear();
+        APIService.currentMaitresse = null;
+        APIService.currentEleve = null;
+        APIService.token = null;
+        RoutingService.isLoggedIn = false;
+        RoutingService.adminMode = false;
+        RoutingService.eleveConnected = false;
+
+        this.router.navigate(['/'])
+    }
 }
