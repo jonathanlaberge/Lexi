5efc57ab9da299b733a44346edf5b0b0f12024c4
Jonathan Laberge <pm@jolab.me>
2019-06-24 11:31:50 -0400

    Curseur Simple Progress

--- Lexi/src/app/ui/curseurs/curseur-simple/curseur-simple.component.ts
+++ Lexi/src/app/ui/curseurs/curseur-simple/curseur-simple.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import $ from 'jquery';
+import 'jquery-ui-dist/jquery-ui';
 
-@Component({
-  selector: 'app-curseur-simple',
-  templateUrl: './curseur-simple.component.html',
-  styleUrls: ['./curseur-simple.component.css']
-})
-export class CurseurSimpleComponent implements OnInit {
+@Component(
+    {
+        selector: 'curseur-simple',
+        templateUrl: './curseur-simple.component.html',
+        styleUrls: ['./curseur-simple.component.css']
+    })
+export class CurseurSimpleComponent implements OnInit
+{
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit()
+    {
+        $("#picker1").slider(
+            {
+                range: "max",
+                min: 0,
+                max: 3,
+                create: function ()
+                {
+                    $("#pickerNumber1").text($(this).slider("value"));
+                    //reponsesNumber.set("picker1", $(this).slider("value"));
+                },
+                slide: function (event, ui) 
+                {
+                    $("#pickerNumber1").text(ui.value);
+                    //reponsesNumber.set("picker1", ui.value);
+                    //CheckAllSlider();
+                }
+            });
+    }
 
 }
