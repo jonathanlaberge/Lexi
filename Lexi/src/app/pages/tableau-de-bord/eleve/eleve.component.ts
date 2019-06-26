6b3a9e9a77aa90ad3dcd298d4d8c0ce3c693f46f
Jonathan Laberge <pm@jolab.me>
2019-06-26 11:20:25 -0400

    Admin - Eleve Dashboard QOS

--- Lexi/src/app/pages/tableau-de-bord/eleve/eleve.component.ts
+++ Lexi/src/app/pages/tableau-de-bord/eleve/eleve.component.ts
@@ -1,136 +1,119 @@
 import { Component, OnInit, ChangeDetectorRef, ViewChild } from '@angular/core';
 import { Eleve } from 'src/app/model/eleve';
 import { APIService } from 'src/app/service/api.service';
-import { RoutingService } from 'src/app/service/routing.service';
 import { ClrForm } from '@clr/angular';
-import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 
-@Component({
-    selector: 'app-eleve',
-    templateUrl: './eleve.component.html',
-    styleUrls: ['./eleve.component.css']
-})
-export class EleveComponent implements OnInit {
-
-
+@Component(
+    {
+        selector: 'app-eleve',
+        templateUrl: './eleve.component.html',
+        styleUrls: ['./eleve.component.css']
+    })
+export class EleveComponent implements OnInit
+{
     @ViewChild(ClrForm, { static: true }) editFormValidator;
-
-
-
-
-
-    selectedEleve: Eleve = null;
-    isReady: boolean = false;
-
-
-    constructor(
-        private apiService: APIService,
-        private ref: ChangeDetectorRef,
-        private formBuilder: FormBuilder,
-        private router: Router,
-        private activeRoute: ActivatedRoute
     
-    ) { }
-
-
     elevesList: Eleve[] = [];
+    selectedEleve: Eleve = null;
 
     isEditModalOpen: boolean = false;
     isDeleteModalOpen: boolean = false;
 
-    modifyForm: FormGroup;
-
+    editForm: FormGroup;
     
     successEdit: boolean = false;
     successDelete: boolean = false;
 
     errorRegisterServer: boolean = false;
-   
-
-
-
-    ngOnInit() {
-
-
-        this.modifyForm = this.formBuilder.group(
+    isReady: boolean = false;
+    
+    constructor(
+        private apiService: APIService,
+        private ref: ChangeDetectorRef,
+        private formBuilder: FormBuilder) { }
+    
+    ngOnInit()
+    {
+        this.editForm = this.formBuilder.group(
             {
                 prenom: ['', [Validators.required]],
                 nom: ['', [Validators.required]],
                 genre: ['', [Validators.required]],
                 dateNaissance: ['', []],
                 avatar: ['', []]
-               
+
             });
 
+        this.GetEleveList();
+    }
 
-
-
-
-        this.apiService.GetEleveList().subscribe((data: any) => {
+    GetEleveList()
+    {
+        this.isReady = false;
+        this.apiService.GetEleveList().subscribe((data: any) =>
+        {
             if (data != null)
-                data.forEach(function (value) {
+                data.forEach(function (value)
+                {
                     this.elevesList.push(value as Eleve);
                 }.bind(this));
 
             this.isReady = true;
             this.ref.detectChanges();
+            console.log(this.elevesList);
         });
-
-
     }
 
+    OnEdit(eleve: Eleve)
+    {
+        this.selectedEleve = eleve;
+        this.editForm.patchValue(
+            {
+                prenom: eleve.prenom,
+                nom: eleve.nom,
+                genre: eleve.genre,
+                dateNaissance: eleve.dateNaissance.getDate(),
+                avatar: eleve.avatar
 
+            });
 
-
-
-    selectUser(idEleve: number) {
-        console.log("id selectionné " + idEleve);
-    }
-
-
-
-
-
-    onEdit(user: Eleve) {
-        this.selectedEleve = user;
         this.isEditModalOpen = true;
-        console.log("id selectionné onEdit " + user.idEleve);
- 
-        }
-    
+    }
 
-
-
-    
-    editEleve() {
-
-
-        if (this.modifyForm.invalid) {
+    SubmitEditEleve()
+    {
+        if (this.editForm.invalid)
+        {
             this.editFormValidator.markAsTouched();
         }
-        else {
+        else
+        {
             var eleve: Eleve = new Eleve();
 
 
             eleve.idEleve = this.selectedEleve.idEleve;
-            eleve.prenom = this.modifyForm.value.prenom;
-            eleve.nom = this.modifyForm.value.nom;
-            eleve.dateNaissance = this.modifyForm.value.dateNaissance;
-            eleve.genre = this.modifyForm.value.genre;
-            eleve.avatar = this.modifyForm.value.avatar;
+            eleve.prenom = this.editForm.value.prenom;
+            eleve.nom = this.editForm.value.nom;
+            eleve.dateNaissance = this.editForm.value.dateNaissance;
+            eleve.genre = this.editForm.value.genre;
+            eleve.avatar = this.editForm.value.avatar;
 
 
             this.apiService.EditEleve(eleve).subscribe(
-                (data: any) => {
-                    if (data.code == 200) {
+                (data: any) =>
+                {
+                    if (data.code == 200)
+                    {
                         this.successEdit = true;
                         this.isEditModalOpen = false;
                     }
                     else
                         this.errorRegisterServer = true;
                 },
-                error => {
+                error =>
+                {
                     if (error.status == 401)
                         this.errorRegisterServer = true;
                     else
@@ -144,13 +127,14 @@ export class EleveComponent implements OnInit {
 
 
 
-    
 
 
 
 
 
-    onDelete(user: Eleve) {
+
+    OnDelete(user: Eleve)
+    {
 
         this.selectedEleve = user;
         this.isDeleteModalOpen = true;
@@ -160,18 +144,22 @@ export class EleveComponent implements OnInit {
 
 
 
-    deleteEleve(idEleve: number) {
+    SubmitDeleteEleve(idEleve: number)
+    {
 
         this.apiService.DeleteEleve(idEleve).subscribe(
-            (data: any) => {
-                if (data.code == 200) {
+            (data: any) =>
+            {
+                if (data.code == 200)
+                {
                     this.successDelete = true;
                     this.isDeleteModalOpen = false;
                 }
                 else
                     this.errorRegisterServer = true;
             },
-            error => {
+            error =>
+            {
                 if (error.status == 401)
                     this.errorRegisterServer = true;
                 else
@@ -195,22 +183,13 @@ export class EleveComponent implements OnInit {
 
 
 
-       onHistory(user: Eleve) {
-
-
+    ShowHistory(user: Eleve)
+    {
         console.log("id selectionné history " + user.idEleve);
-
-
     }
-       onPickPlayList(user: Eleve) {
 
-
+    SelectPlayList(user: Eleve)
+    {
         console.log("id selectionné playlist " + user.idEleve);
-
-
     }
-
-
-
 }
-
