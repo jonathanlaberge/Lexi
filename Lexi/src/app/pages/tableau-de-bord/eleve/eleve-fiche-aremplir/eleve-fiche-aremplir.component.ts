28d906d9c62b605cbd47d6a34c70972df1aa85d4
GuillaumeBeaudoin <48021866+GuillaumeBeaudoin@users.noreply.github.com>
2019-06-27 17:23:00 +0200

    in progresse add playlist to student

--- Lexi/src/app/pages/tableau-de-bord/eleve/eleve-fiche-aremplir/eleve-fiche-aremplir.component.ts
+++ Lexi/src/app/pages/tableau-de-bord/eleve/eleve-fiche-aremplir/eleve-fiche-aremplir.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ClrForm } from '@clr/angular';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { APIService } from 'src/app/service/api.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Eleve } from 'src/app/model/eleve';
 
 @Component({
   selector: 'app-eleve-fiche-aremplir',
@@ -7,9 +12,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EleveFicheARemplirComponent implements OnInit {
 
-  constructor() { }
+    @ViewChild(ClrForm, { static: true }) creationFormValidator;
 
-  ngOnInit() {
-  }
+    isCreationModalOpen: boolean = true;
+    isLoadingModal: boolean = false;
 
+    creationForm: FormGroup;
+
+    errorServer: boolean = false;
+
+    constructor(
+        private apiService: APIService,
+        private formBuilder: FormBuilder,
+        private router: Router,
+        private route: ActivatedRoute) { }
+
+    ngOnInit()
+    {
+        this.creationForm = this.formBuilder.group(
+            {
+                prenom: ['', [Validators.required]],
+                nom: ['', [Validators.required]],
+                genre: ['', []],
+                dateNaissance: ['', []],
+                avatar: ['', []]
+            });
+    }
+
+    SubmitCreationForm()
+    {
+        if (this.creationForm.invalid) {
+            this.creationFormValidator.markAsTouched();
+        }
+        else {
+            this.isLoadingModal = true;
+            var eleve: Eleve = new Eleve();
+
+            eleve.prenom = this.creationForm.value.prenom;
+            eleve.nom = this.creationForm.value.nom;
+            eleve.dateNaissance = this.creationForm.value.dateNaissance;
+            eleve.genre = this.creationForm.value.genre;
+            eleve.avatar = this.creationForm.value.avatar;
+
+            this.apiService.AddEleve(eleve).subscribe(
+                (data: any) => {
+                    if (data.code == 200) {
+                        this.isLoadingModal = false;
+                        this.Close();
+                    }
+                    else
+                        this.errorServer = true;
+                },
+                () => {
+                    this.errorServer = true;
+                    this.isLoadingModal = false;
+                });
+        }
+    }
+
+    Close()
+    {
+        this.errorServer = false;
+        this.isLoadingModal = false;
+        this.creationForm.reset();
+        this.router.navigate([`../`], { relativeTo: this.route });
+    }
 }
