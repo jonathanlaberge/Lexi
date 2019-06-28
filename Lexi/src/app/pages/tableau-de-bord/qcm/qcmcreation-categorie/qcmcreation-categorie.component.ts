936397938b878dbcae76d95173c74853511faa02
Jonathan Laberge <pm@jolab.me>
2019-06-28 05:12:32 -0400

    Admin - QCM Categorie CRUD Progress

--- Lexi/src/app/pages/tableau-de-bord/qcm/qcmcreation-categorie/qcmcreation-categorie.component.ts
+++ Lexi/src/app/pages/tableau-de-bord/qcm/qcmcreation-categorie/qcmcreation-categorie.component.ts
@@ -1,114 +1,83 @@
-import { Component, OnInit, ViewChild, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ClrForm } from '@clr/angular';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { APIService } from 'src/app/service/api.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Categorie } from 'src/app/model/categorie';
 
-@Component({
-  selector: 'app-qcmcreation-categorie',
-  templateUrl: './qcmcreation-categorie.component.html',
-  styleUrls: ['./qcmcreation-categorie.component.css']
-})
-export class QCMCreationCategorieComponent implements OnInit {
+@Component(
+    {
+        selector: 'app-qcmcreation-categorie',
+        templateUrl: './qcmcreation-categorie.component.html'
+    })
+export class QCMCreationCategorieComponent implements OnInit
+{
+    @ViewChild(ClrForm, { static: true }) creationFormValidator;
 
+    isCreationModalOpen: boolean = true;
+    isLoadingModal: boolean = false;
 
-    @ViewChild(ClrForm, { static: true }) addCategorieFormValidator;
+    creationForm: FormGroup;
 
-    isAddModalOpen: boolean = true;
-
-
-
-
-
-
-    addCategorieForm: FormGroup;
-
-
-    successRegister: boolean = false;
-    errorRegisterServer: boolean = false;
-
-    successAdd: boolean = false;
-    errorAddServer: boolean = false;
-
-
+    errorServer: boolean = false;
 
     constructor(
         private apiService: APIService,
-        private ref: ChangeDetectorRef,
         private formBuilder: FormBuilder,
         private router: Router,
-        private route: ActivatedRoute
+        private route: ActivatedRoute) { }
 
-    ) { }
-
-    ngOnInit() {
-
-
-        this.addCategorieForm = this.formBuilder.group(
+    ngOnInit()
+    {
+        this.creationForm = this.formBuilder.group(
             {
                 nom: ['', [Validators.required]],
                 matiere: ['', [Validators.required]],
-                niveau: ['',[]],
+                niveau: ['', [Validators.required]],
                 estPublic: ['', [Validators.required]]
-
             });
     }
 
-    
-
-    submitAddCategorieForm() {
-
-
-        if (this.addCategorieForm.invalid) {
-            this.addCategorieFormValidator.markAsTouched();
+    SubmitCreationForm()
+    {
+        if (this.creationForm.invalid)
+        {
+            this.creationFormValidator.markAsTouched();
         }
-        else {
+        else
+        {
+            this.isLoadingModal = true;
             var categorie: Categorie = new Categorie();
 
-
-
-           // categorie.idCategorie = this.addCategorieForm.value.idCategorie;
-            categorie.nom = this.addCategorieForm.value.nom;
-            categorie.matiere = this.addCategorieForm.value.matiere;
-            categorie.niveau = this.addCategorieForm.value.niveau;
-            categorie.estPublic = this.addCategorieForm.value.estPublic;
-            //categorie.idMaitresseCreatrice = this.addCategorieForm.value.idMaitresseCreatrice;
+            categorie.nom = this.creationForm.value.nom;
+            categorie.matiere = this.creationForm.value.matiere;
+            categorie.niveau = this.creationForm.value.niveau;
+            categorie.estPublic = this.creationForm.value.estPublic;
 
             this.apiService.AddCategorie(categorie).subscribe(
-                (data: any) => {
-                    if (data.code == 200) {
-                        this.successRegister = true;
-                        this.cancel();
-
+                (data: any) =>
+                {
+                    if (data.code == 200)
+                    {
+                        this.isLoadingModal = false;
+                        this.Close();
                     }
                     else
-                        this.errorRegisterServer = true;
+                        this.errorServer = true;
                 },
-                error => {
-                    if (error.status == 401)
-                        this.errorRegisterServer = true;
-                    else
-                        this.errorRegisterServer = true;
+                () =>
+                {
+                    this.errorServer = true;
+                    this.isLoadingModal = false;
                 });
-
-
         }
-
-
-
-
     }
 
-    cancel() {
-        this.addCategorieForm.reset();
+    Close()
+    {
+        this.errorServer = false;
+        this.isLoadingModal = false;
+        this.creationForm.reset();
         this.router.navigate([`../`], { relativeTo: this.route });
     }
-
-
-
-
-
-
-
 }
--- Lexi/src/app/pages/tableau-de-bord/qcm/qcmmodification-categorie/qcmmodification-categorie.component.ts
+++ Lexi/src/app/pages/tableau-de-bord/qcm/qcmmodification-categorie/qcmmodification-categorie.component.ts
@@ -1,15 +1,94 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ClrForm } from '@clr/angular';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { APIService } from 'src/app/service/api.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Categorie } from 'src/app/model/categorie';
 
-@Component({
-  selector: 'app-qcmmodification-categorie',
-  templateUrl: './qcmmodification-categorie.component.html',
-  styleUrls: ['./qcmmodification-categorie.component.css']
-})
-export class QCMModificationCategorieComponent implements OnInit {
+@Component(
+    {
+        selector: 'app-qcmmodification-categorie',
+        templateUrl: './qcmmodification-categorie.component.html'
+    })
+export class QCMModificationCategorieComponent implements OnInit
+{
+    @ViewChild(ClrForm, { static: true }) editFormValidator;
 
-  constructor() { }
+    isEditModalOpen: boolean = true;
+    isLoadingModal: boolean = false;
+    isReady: boolean = false;
 
-  ngOnInit() {
-  }
+    editForm: FormGroup;
 
+    errorServer: boolean = false;
+
+    constructor(
+        private apiService: APIService,
+        private formBuilder: FormBuilder,
+        private router: Router,
+        private route: ActivatedRoute) { }
+
+    ngOnInit()
+    {
+        this.editForm = this.formBuilder.group(
+            {
+                nom: ['', [Validators.required]],
+                matiere: ['', [Validators.required]],
+                niveau: ['', [Validators.required]],
+                estPublic: ['', [Validators.required]]
+            });
+
+        this.route.params.subscribe(params =>
+        {
+            if (!isNaN(parseFloat(params['id'])))
+            {
+                //this.apiService.///////////////////////////////////////////////////
+            }
+            else
+                this.router.navigate([`../`], { relativeTo: this.route });
+        });
+    }
+
+    SubmitEditForm()
+    {
+        if (this.editForm.invalid)
+        {
+            this.editFormValidator.markAsTouched();
+        }
+        else
+        {
+            this.isLoadingModal = true;
+            var categorie: Categorie = new Categorie();
+
+            categorie.nom = this.editForm.value.nom;
+            categorie.matiere = this.editForm.value.matiere;
+            categorie.niveau = this.editForm.value.niveau;
+            categorie.estPublic = this.editForm.value.estPublic;
+
+            this.apiService.AddCategorie(categorie).subscribe(
+                (data: any) =>
+                {
+                    if (data.code == 200)
+                    {
+                        this.isLoadingModal = false;
+                        this.Close();
+                    }
+                    else
+                        this.errorServer = true;
+                },
+                () =>
+                {
+                    this.errorServer = true;
+                    this.isLoadingModal = false;
+                });
+        }
+    }
+
+    Close()
+    {
+        this.errorServer = false;
+        this.isLoadingModal = false;
+        this.editForm.reset();
+        this.router.navigate([`../../`], { relativeTo: this.route });
+    }
 }
