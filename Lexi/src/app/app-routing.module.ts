d0157930d9c134e7a512fa0d810384cb821ad4bd
Jonathan Laberge <pm@jolab.me>
2019-06-28 04:25:47 -0400

    Update app-routing.module.ts

--- Lexi/src/app/app-routing.module.ts
+++ Lexi/src/app/app-routing.module.ts
@@ -6,13 +6,13 @@ import { TableauDeBordComponent } from './pages/tableau-de-bord/tableau-de-bord.
 import { EleveComponent } from './pages/tableau-de-bord/eleve/eleve.component';
 import { EleveCreationComponent } from './pages/tableau-de-bord/eleve/eleve-creation/eleve-creation.component';
 import { EleveFicheARemplirComponent } from './pages/tableau-de-bord/eleve/eleve-fiche-aremplir/eleve-fiche-aremplir.component';
-import { EleveStatistiqueComponent } from './pages/tableau-de-bord/eleve/eleve-statistique/eleve-statistique.component';
+//import { EleveStatistiqueComponent } from './pages/tableau-de-bord/eleve/eleve-statistique/eleve-statistique.component';
 import { QCMComponent } from './pages/tableau-de-bord/qcm/qcm.component';
 import { QCMCreationCategorieComponent } from './pages/tableau-de-bord/qcm/qcmcreation-categorie/qcmcreation-categorie.component';
 import { QCMCreationFicheComponent } from './pages/tableau-de-bord/qcm/qcmcreation-fiche/qcmcreation-fiche.component';
 import { QCMModificationCategorieComponent } from './pages/tableau-de-bord/qcm/qcmmodification-categorie/qcmmodification-categorie.component';
 import { QCMModificationFicheComponent } from './pages/tableau-de-bord/qcm/qcmmodification-fiche/qcmmodification-fiche.component';
-import { QCMStatistiqueComponent } from './pages/tableau-de-bord/qcm/qcmstatistique/qcmstatistique.component';
+//import { QCMStatistiqueComponent } from './pages/tableau-de-bord/qcm/qcmstatistique/qcmstatistique.component';
 import { EleveGuard } from './service/eleve.guard';
 import { TableauDeBordEleveComponent } from './pages/tableau-de-bord-eleve/tableau-de-bord-eleve.component';
 import { EleveFicheComponent } from './pages/tableau-de-bord-eleve/eleve-fiche/eleve-fiche.component';
@@ -44,8 +44,8 @@ const routes: Routes =
                                 { path: 'creation', component: EleveCreationComponent },
                                 { path: 'fichearemplir', component: EleveFicheARemplirComponent },
                                 { path: 'fichearemplir/:id', component: EleveFicheARemplirComponent },
-                                { path: 'statistique', component: EleveStatistiqueComponent },
-                                { path: 'statistique/:id', component: EleveStatistiqueComponent }
+                                //{ path: 'statistique', component: EleveStatistiqueComponent },
+                                //{ path: 'statistique/:id', component: EleveStatistiqueComponent }
                             ]
                     },
                     {
@@ -55,12 +55,13 @@ const routes: Routes =
                             [
                                 { path: 'creationcategorie', component: QCMCreationCategorieComponent },
                                 { path: 'creationfiche', component: QCMCreationFicheComponent },
+                                { path: 'creationfiche/:id', component: QCMCreationFicheComponent },
                                 { path: 'modificationcategorie', component: QCMModificationCategorieComponent },
                                 { path: 'modificationcategorie/:id', component: QCMModificationCategorieComponent },
                                 { path: 'modificationfiche', component: QCMModificationFicheComponent },
                                 { path: 'modificationfiche/:id', component: QCMModificationFicheComponent },
-                                { path: 'statistique', component: QCMStatistiqueComponent },
-                                { path: 'statistique/:id', component: QCMStatistiqueComponent }
+                                //{ path: 'statistique', component: QCMStatistiqueComponent },
+                                //{ path: 'statistique/:id', component: QCMStatistiqueComponent }
                             ]
                     },
                 ]
