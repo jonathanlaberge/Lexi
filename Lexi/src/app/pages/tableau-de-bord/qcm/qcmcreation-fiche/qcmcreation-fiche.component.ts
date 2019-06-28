67a24b2c8855074a5d9170010677ae32c5b09b71
Jonathan Laberge <pm@jolab.me>
2019-06-28 10:58:49 -0400

    Admin - QCM Create Fiche Progression And Some Small Change

--- Lexi/src/app/pages/tableau-de-bord/qcm/qcmcreation-fiche/qcmcreation-fiche.component.ts
+++ Lexi/src/app/pages/tableau-de-bord/qcm/qcmcreation-fiche/qcmcreation-fiche.component.ts
@@ -1,15 +1,73 @@
 import { Component, OnInit } from '@angular/core';
+import { APIService } from 'src/app/service/api.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Fiche } from 'src/app/model/fiche';
 
-@Component({
-  selector: 'app-qcmcreation-fiche',
-  templateUrl: './qcmcreation-fiche.component.html',
-  styleUrls: ['./qcmcreation-fiche.component.css']
-})
-export class QCMCreationFicheComponent implements OnInit {
+@Component(
+    {
+        selector: 'app-qcmcreation-fiche',
+        templateUrl: './qcmcreation-fiche.component.html',
+        styleUrls: ['./qcmcreation-fiche.component.css']
+    })
+export class QCMCreationFicheComponent implements OnInit
+{
+    isCreationModalOpen: boolean = true;
+    isLoadingModal: boolean = false;
 
-  constructor() { }
+    
 
-  ngOnInit() {
-  }
+    errorServer: boolean = false;
+    
+    constructor(
+        private apiService: APIService,
+        private router: Router,
+        private route: ActivatedRoute) { }
 
+    ngOnInit()
+    {
+
+    }
+
+
+    SubmitCreationForm()
+    {
+        if (/*this.creationForm.invalid*/false)
+        {
+            //this.creationFormValidator.markAsTouched();
+        }
+        else
+        {
+            this.isLoadingModal = true;
+            var fiche: Fiche = new Fiche();
+
+            //categorie.nom = this.creationForm.value.nom;
+            //categorie.matiere = this.creationForm.value.matiere;
+            //categorie.niveau = this.creationForm.value.niveau;
+            //categorie.estPublic = this.creationForm.value.estPublic;
+
+            this.apiService.AddCategorie(fiche).subscribe(
+                (data: any) =>
+                {
+                    if (data.code == 200)
+                    {
+                        this.isLoadingModal = false;
+                        this.Close();
+                    }
+                    else
+                        this.errorServer = true;
+                },
+                () =>
+                {
+                    this.errorServer = true;
+                    this.isLoadingModal = false;
+                });
+        }
+    }
+
+    Close()
+    {
+        this.errorServer = false;
+        this.isLoadingModal = false;
+        this.router.navigate([`../`], { relativeTo: this.route });
+    }
 }
