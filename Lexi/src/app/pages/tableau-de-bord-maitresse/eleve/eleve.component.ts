df84a8027424261aaf29f53addfa02022e3dc643
Jonathan Laberge <pm@jolab.me>
2019-07-02 10:27:01 -0400

    Admin Avatar QOS

--- Lexi/src/app/pages/tableau-de-bord-maitresse/eleve/eleve.component.ts
+++ Lexi/src/app/pages/tableau-de-bord-maitresse/eleve/eleve.component.ts
@@ -14,7 +14,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class EleveComponent implements OnInit
 {
     @ViewChild(ClrForm, { static: true }) editFormValidator;
-    
+
     elevesList: Eleve[] = [];
     selectedEleve: Eleve = null;
 
@@ -23,33 +23,31 @@ export class EleveComponent implements OnInit
 
     editForm: FormGroup;
     
-
     errorServer: boolean = false;
 
     isLoadingModal: boolean = false;
     isLoading: boolean = false;
-
-
-    images: any[];
+    
+    avatars: any[];
     avatarPathIndex: number;
     selectedAvatarPath: string;
-    
+
     constructor(
         private apiService: APIService,
         private ref: ChangeDetectorRef,
         private formBuilder: FormBuilder,
         private router: Router,
         private route: ActivatedRoute) { }
-    
-    ngOnInit() {
-    
+
+    ngOnInit()
+    {
+
         this.editForm = this.formBuilder.group(
             {
-                prenom: [ [Validators.required]],
-                nom: [ [Validators.required]],
-                genre: [ [Validators.required]],
-                dateNaissance: ['', []],
-                avatar: ['', []]
+                prenom: [[Validators.required]],
+                nom: [[Validators.required]],
+                genre: [[Validators.required]],
+                dateNaissance: ['', []]
             });
 
         this.GetEleveList(0);
@@ -66,49 +64,54 @@ export class EleveComponent implements OnInit
                 {
                     this.elevesList.push(value as Eleve);
                 }.bind(this));
-            
+
             this.isLoading = false;
             this.ref.detectChanges();
         });
     }
-    
+
 
     OnEdit(eleve: Eleve)
     {
-        
-
-        this.images = [];
-
         this.selectedEleve = eleve;
         this.editForm.setValue(
             {
                 prenom: eleve.prenom,
                 nom: eleve.nom,
                 genre: eleve.genre,
-                dateNaissance: eleve.dateNaissance,
-                avatar: ""
+                dateNaissance: eleve.dateNaissance
             });
-
-        this.images.push({ source: this.selectedEleve.avatar, alt: 'Avatar courrent', title: 'Avatar courrent' });
-
-        this.getAvatar(0);
-        for (var i = 1; i <= 22; i++) {
-            this.images.push({ source: 'assets/kids-avatars/png/boy-' + i + '.png', alt: 'Avatar garcon ' + 1, title: 'Garcon ' + i });
-        }
-
-        for (var i = 1; i <= 26; i++) {
-            this.images.push({ source: 'assets/kids-avatars/png/girl-' + i + '.png', alt: 'Avatar fille ' + 1, title: 'Fille ' + i });
-        }
+        
+        this.SetupAvatarList({ source: this.selectedEleve.avatar, alt: 'Avatar actuel', title: 'Avatar actuel' });
+        this.SetChosenAvatar(0);
+        
         this.errorServer = false;
         this.isEditModalOpen = true;
     }
 
 
-    getAvatar(index: number) {
-        console.log(this.images[index].source)
-        this.selectedAvatarPath = this.images[index].source;
+    SetChosenAvatar(index: number)
+    {
+        this.selectedAvatarPath = this.avatars[index].source;
     }
 
+    SetupAvatarList(firstAvatar: any = null)
+    {
+        this.avatars = [];
+
+        if (firstAvatar != null)
+            this.avatars.push(firstAvatar);
+
+        for (var i = 0; i <= 22; i++)
+        {
+            this.avatars.push({ source: 'assets/kids-avatars/png/boy-' + i + '.png', alt: 'Avatar Gars ' + 1, title: 'Gars ' + i });
+        }
+
+        for (var i = 0; i <= 26; i++)
+        {
+            this.avatars.push({ source: 'assets/kids-avatars/png/girl-' + i + '.png', alt: 'Avatar Gille ' + 1, title: 'Fille ' + i });
+        }
+    }
 
     SubmitEditEleve()
     {
@@ -125,7 +128,8 @@ export class EleveComponent implements OnInit
             eleve.nom = this.editForm.value.nom;
             eleve.dateNaissance = this.editForm.value.dateNaissance;
             eleve.genre = this.editForm.value.genre;
-            if (this.editForm.value.selectedAvatarPath != "")
+
+            if (this.selectedAvatarPath != "")
                 eleve.avatar = this.selectedAvatarPath;
 
             this.isLoadingModal = true;
@@ -150,7 +154,7 @@ export class EleveComponent implements OnInit
                 });
         }
     }
-    
+
     OnDelete(eleve: Eleve)
     {
         this.selectedEleve = eleve;
@@ -158,7 +162,7 @@ export class EleveComponent implements OnInit
         this.errorServer = false;
         this.isDeleteModalOpen = true;
     }
-    
+
     SubmitDeleteEleve(idEleve: number)
     {
         this.isLoadingModal = true;
@@ -182,7 +186,7 @@ export class EleveComponent implements OnInit
                 this.errorServer = true;
             });
     }
-    
+
     //ShowHistory(user: Eleve)
     //{
     //    console.log("id selectionné history " + user.idEleve);
@@ -192,11 +196,4 @@ export class EleveComponent implements OnInit
     {
         this.router.navigate(['fichearemplir', user.idEleve], { relativeTo: this.route });
     }
-
-
-
-
-
-
-
 }
