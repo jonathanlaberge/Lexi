4d4e9707fa187cf5875c27a14c04c34d30c4fba0
Jonathan Laberge <pm@jolab.me>
2019-07-03 03:52:34 -0400

    Eleve - QCM Progress

--- Lexi/src/app/pages/tableau-de-bord-eleve/eleve-qcm/eleve-qcm.component.ts
+++ Lexi/src/app/pages/tableau-de-bord-eleve/eleve-qcm/eleve-qcm.component.ts
@@ -21,42 +21,37 @@ export class EleveQCMComponent implements OnInit
     isReady: boolean = false;
 
     reponseList = new Map();
-    
+    isDisable: boolean;
+    isLoading: boolean;
+    errorServer: boolean;
+
     constructor(
         private apiService: APIService,
         private ref: ChangeDetectorRef,
         private router: Router,
         private activeRoute: ActivatedRoute) { }
-    
+
     ngOnInit()
     {
         this.activeRoute.params.subscribe(params =>
         {
             if (!isNaN(parseFloat(params['idCategorie'])) && !isNaN(parseFloat(params['idFiche'])))
             {
-                this.idCategorie = params['idCategorie'];
-                this.idFiche = params['idFiche'];
+                this.apiService.UserController_FicheGet(params['idCategorie'], params['idFiche']).subscribe(
+                    (data) =>
+                    {
+                        this.fiche = data as Fiche;
+                        
+                        this.isReady = true;
+                        this.ref.detectChanges();
+                    },
+                    () =>
+                    {
+                        this.errorServer = true;
+                    });
             }
-        });
-
-
-
-
-        this.apiService.UserController_FicheGet(this.idCategorie, this.idFiche).subscribe((data: any) =>
-        {
-            if (data != null)
-            {
-                this.titre = data.titre;
-
-                data.listeQuestion.forEach(function (value)
-                {
-                    value.choixDeReponses = JSON.parse(value.choixDeReponses)
-                    this.listQuestion.push(value as Question);
-                }.bind(this));
-            }
-
-            this.isReady = true;
-            this.ref.detectChanges();
+            else
+                this.router.navigate([`/eleve/fiche`]);
         });
     }
 
@@ -64,108 +59,104 @@ export class EleveQCMComponent implements OnInit
 
 
 
-    validationDesSliders(reponseSlider: number, idReponse: number)
-    {
-        console.log({ i: reponseSlider, j: idReponse });
+    //validationDesSliders(reponseSlider: number, idReponse: number)
+    //{
+    //    console.log({ i: reponseSlider, j: idReponse });
 
-        this.reponseList.set(idReponse, reponseSlider);
+    //    this.reponseList.set(idReponse, reponseSlider);
 
-        // activation du boutton ou non selon une réponse non selectionnée.
-        for (let value of this.reponseList.values())
-        {
-            if (value == 0)
-            {
-                this.isDesable = true;
-                break;
-            } else
-                this.isDesable = false;
-        }
-    }
+    //    // activation du boutton ou non selon une réponse non selectionnée.
+    //    for (let value of this.reponseList.values())
+    //    {
+    //        if (value == 0)
+    //        {
+    //            this.isDisable = true;
+    //            break;
+    //        }
+    //        else
+    //            this.isDisable = false;
+    //    }
+    //}
 
-
-
-
-
-
-    validation()
-    {
-        console.log("this.reponseList " + this.reponseList.size);
-        /* "idFiche": 19,
-             "idCategorie": 4,
-                 "listeQuestion": [3, 3, 2, 3]
+    //validation()
+    //{
+    //    console.log("this.reponseList " + this.reponseList.size);
+    //    /* "idFiche": 19,
+    //         "idCategorie": 4,
+    //             "listeQuestion": [3, 3, 2, 3]
 
 
-        this.idCategorie
-        this.idFiche*/
+    //    this.idCategorie
+    //    this.idFiche*/
 
 
 
-        var listeReponse: number[] = [];
+    //    var listeReponse: number[] = [];
 
 
-        for (let i = 1; i <= this.reponseList.size; i++)
-        {
-            listeReponse.push(this.reponseList.get(i))
-        }
+    //    for (let i = 1; i <= this.reponseList.size; i++)
+    //    {
+    //        listeReponse.push(this.reponseList.get(i))
+    //    }
 
 
 
-        this.apiService.UserController_FicheValidation({ idCategorie: this.idCategorie, idFiche: this.idFiche, listeReponseEleve: listeReponse }).subscribe(
-            (data: any) =>
-            {
-                if (data != null)
-                {
+    //    this.apiService.UserController_FicheValidation({ idCategorie: this.idCategorie, idFiche: this.idFiche, listeReponseEleve: listeReponse }).subscribe(
+    //        (data: any) =>
+    //        {
+    //            if (data != null)
+    //            {
 
 
-                    console.log(data);
-                    data.correction
+    //                console.log(data);
+    //                data.correction
 
 
-                    /*this.color[0] = QCMColor.Green;
-                    this.color[1] = QCMColor.Red;*/
-                    // s'il n'y a pas d'erreur 
-                    if (data.nombreErreur == 0)
-                    {
+    //                /*this.color[0] = QCMColor.Green;
+    //                this.color[1] = QCMColor.Red;*/
+    //                // s'il n'y a pas d'erreur 
+    //                if (data.nombreErreur == 0)
+    //                {
 
-                        window.alert("Bravo, 100%");
-                        this.router.navigate([`../`], { relativeTo: this.route });
-                    }
-
-
-                    // s'il y a des erreurs
-                    for (let i = 1; i <= this.reponseList.size; i++)
-                    {
-                        if (data.bonnesReponses[i] == true)
-                        {
-                            console.log("question [" + (i) + "] true");
-                        } else
-                        {
-                            this.color[1]
-                            console.log("question [" + (i) + "] false");
-                        }
+    //                    window.alert("Bravo, 100%");
+    //                    this.router.navigate([`../`], { relativeTo: this.route });
+    //                }
 
 
-                    }
-
-                    window.alert("Erreur trouvé.");
-                    this.router.navigate([`../`], { relativeTo: this.route });
-
-                }
-                else
-                    this.errorRegisterServer = true;
-            },
-            error =>
-            {
-                if (error.status == 401)
-                    this.errorRegisterServer = true;
-                else
-                    this.errorRegisterServer = true;
-            });
+    //                // s'il y a des erreurs
+    //                for (let i = 1; i <= this.reponseList.size; i++)
+    //                {
+    //                    if (data.bonnesReponses[i] == true)
+    //                    {
+    //                        console.log("question [" + (i) + "] true");
+    //                    } else
+    //                    {
+    //                        this.color[1]
+    //                        console.log("question [" + (i) + "] false");
+    //                    }
 
 
-        console.log("this.keys " + listeReponse);
+    //                }
 
-    }
+    //                window.alert("Erreur trouvé.");
+    //                this.router.navigate([`../`], { relativeTo: this.route });
+
+    //            }
+    //            else
+    //                this.errorRegisterServer = true;
+    //        },
+    //        error =>
+    //        {
+    //            if (error.status == 401)
+    //                this.errorRegisterServer = true;
+    //            else
+    //                this.errorRegisterServer = true;
+    //        });
+
+
+    //    console.log("this.keys " + listeReponse);
+
+    //}
 }
 /*
      * envoie
