be20fe134e227d1e6a4cbfc2043740a4a3d4b416
GuillaumeBeaudoin <48021866+GuillaumeBeaudoin@users.noreply.github.com>
2019-07-05 14:36:29 +0200

    :100: :100: :100:  terminer :100: :100: :100:

--- Lexi/src/app/ui/dashboard/navbar/navbar.component.ts
+++ Lexi/src/app/ui/dashboard/navbar/navbar.component.ts
@@ -4,7 +4,7 @@ import { RoutingService } from 'src/app/service/routing.service';
 import { APIService } from 'src/app/service/api.service';
 import { Router } from '@angular/router';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
-import { ClrForm } from '@clr/angular';
+import { ClrForm, fade } from '@clr/angular';
 import { Maitresse } from 'src/app/model/maitresse';
 
 @Component(
@@ -12,8 +12,7 @@ import { Maitresse } from 'src/app/model/maitresse';
         selector: 'dashboard-navbar',
         templateUrl: './navbar.component.html'
     })
-export class NavbarComponent implements OnInit, OnDestroy
-{
+export class NavbarComponent implements OnInit, OnDestroy {
 
 
     isAdmin: boolean = false;
@@ -22,6 +21,7 @@ export class NavbarComponent implements OnInit, OnDestroy
 
     isEditModalOpen: boolean = false;
     errorServer: boolean = false;
+
 
 
     errorLogin: boolean = false;
@@ -36,19 +36,23 @@ export class NavbarComponent implements OnInit, OnDestroy
     isLoadingModal: boolean = false;
     prenom: string;
     nom: string;
+
+
+    motdepasse2: string;
     motdepasse: string;
-    confirmationmotdepasse: string;
+    mismatch: boolean = true;
+    passwordLenght: boolean = false;
+    currentPasswordError: boolean = false;
 
-    constructor(private router: Router, private formBuilder: FormBuilder,private apiService: APIService) { }
 
-    ngOnInit()
-    {
+    constructor(private router: Router, private formBuilder: FormBuilder, private apiService: APIService) { }
+
+    ngOnInit() {
         this.isAdmin = RoutingService.adminMode;
         this.eleveConnected = RoutingService.eleveConnected;
         this.name = `${APIService.currentMaitresse.prenom} ${APIService.currentMaitresse.nom}`;
 
-        this.subscriptionRoutingService = RoutingService.routeSubject.subscribe(() => 
-        {
+        this.subscriptionRoutingService = RoutingService.routeSubject.subscribe(() => {
             this.isAdmin = RoutingService.adminMode;
             this.eleveConnected = RoutingService.eleveConnected;
             this.name = `${APIService.currentMaitresse.prenom} ${APIService.currentMaitresse.nom}`;
@@ -63,33 +67,40 @@ export class NavbarComponent implements OnInit, OnDestroy
 
 
 
-    
-    ngOnDestroy()
-    {
+
+    ngOnDestroy() {
         if (this.subscriptionRoutingService != null)
             this.subscriptionRoutingService.unsubscribe();
     }
 
-    ShowProfile()
-    {
-       // console.log(APIService.token);
+    ShowProfile() {
+        // console.log(APIService.token);
 
 
-         
-               
+
+
         this.prenom = APIService.currentMaitresse.prenom;
-                   
+
         this.nom = APIService.currentMaitresse.nom;
-                
 
 
 
-            this.errorServer = false;
-            this.isEditModalOpen = true;
+
+        this.errorServer = false;
+        this.isEditModalOpen = true;
     }
 
 
+    closeEdit() {
 
+
+        this.motdepasseConfirmation = "";
+        this.motdepasse = "";
+        this.motdepasse2 = "";
+        this.isEditModalOpen = false;
+
+
+    }
 
 
 
@@ -103,29 +114,36 @@ export class NavbarComponent implements OnInit, OnDestroy
         maitresse.idMaitresse = APIService.currentMaitresse.idMaitresse;
         maitresse.nom = this.nom;
         maitresse.prenom = this.prenom;
-        maitresse.motdepasse = this.confirmationmotdepasse;
-
+        maitresse.motdepasseConfirmation = this.motdepasseConfirmation;
+        maitresse.motdepasse = this.motdepasse;
         console.log(maitresse)
 
 
-        if (this.confirmationmotdepasse.length <= 8)
-            this.errorServer = false;
-        else       
-            this.apiService.AccountController_ProfilUpdate(maitresse).subscribe(
-                    (data: any) => {
-                        this.isLoadingModal = false;
-                        if (data.code == 200) {
-                           
-                            this.isEditModalOpen = false;
-                        }
-                        else
-                            this.errorServer = true;
-                    },
-                    () => {
-                        this.isLoadingModal = false;
+
+        this.apiService.AccountController_ProfilUpdate(maitresse).subscribe(
+            (data: any) => {
+                this.isLoadingModal = false;
+                if (data.code == 200) {
+                    this.closeEdit();
+                }
+                else
+                    this.errorServer = true;
+            },
+            (error) => {
+                this.isLoadingModal = false;
+                switch (error.status) {
+
+                    case 401: this.currentPasswordError = true;
+                        break;
+                    default:
+
                         this.errorServer = true;
-                    });
-        
+
+                        break;
+
+                }
+            });
+
     }
 
 
@@ -138,41 +156,37 @@ export class NavbarComponent implements OnInit, OnDestroy
 
 
 
-    GoToTableauDeBord()
-    {
+    GoToTableauDeBord() {
         if (APIService.IsTokenInAdminMode())
             this.router.navigate(['/tableaudebord']);
         else
             this.showLoginModal = true;
     }
 
-    SubmitAdminMode()
-    {
+    SubmitAdminMode() {
         var args = { motdepasse: this.loginPassword };
 
         this.apiService.AccountController_Mode(args).subscribe(
-            (data: any) =>
-            {
-                if (data.token != null)
-                {
+            (data: any) => {
+                if (data.token != null) {
                     APIService.token = data.token;
                     localStorage.setItem('token', JSON.stringify(APIService.token));
                     RoutingService.adminMode = true;
                     RoutingService.eleveConnected = false;
                     RoutingService.SetRouteToAdmin();
+
+
                     this.errorServer = false;
                     this.loginIsReady = true;
                     this.router.navigate(['/tableaudebord']);
                 }
-                else
-                {
+                else {
                     this.errorServer = true;
                     this.loginIsReady = true;
                 }
-                
+
             },
-            error =>
-            {
+            error => {
                 if (error.status == 401)
                     this.errorLogin = true;
                 else
@@ -182,15 +196,44 @@ export class NavbarComponent implements OnInit, OnDestroy
             });
     }
 
-    GoToElevePortail()
-    {
+    GoToElevePortail() {
         this.router.navigate(['/eleve']);
     }
 
-    Logout()
-    {
+    Logout() {
         RoutingService.Logout();
 
         this.router.navigate(['/']);
     }
+
+
+
+
+
+
+
+
+
+
+
+
+    confirmPassword() {
+
+        if (this.motdepasseConfirmation.length <= 8)
+            this.passwordLenght = true;
+        else
+            this.passwordLenght = false;
+
+        if (this.motdepasse2 != this.motdepasse) {
+
+            console.log(" error ");
+            this.mismatch = true;
+
+        } else {
+            this.mismatch = false;
+        }
+
+
+    }
+
 }
