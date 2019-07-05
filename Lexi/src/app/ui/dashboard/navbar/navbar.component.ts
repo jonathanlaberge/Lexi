35887f5e2e7abc221c03d8ab956678f778fb701a
GuillaumeBeaudoin <48021866+GuillaumeBeaudoin@users.noreply.github.com>
2019-07-05 11:49:57 +0200

    modification du profile

--- Lexi/src/app/ui/dashboard/navbar/navbar.component.ts
+++ Lexi/src/app/ui/dashboard/navbar/navbar.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { RoutingService } from 'src/app/service/routing.service';
 import { APIService } from 'src/app/service/api.service';
 import { Router } from '@angular/router';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { ClrForm } from '@clr/angular';
+import { Maitresse } from 'src/app/model/maitresse';
 
 @Component(
     {
@@ -11,21 +14,32 @@ import { Router } from '@angular/router';
     })
 export class NavbarComponent implements OnInit, OnDestroy
 {
+
+
     isAdmin: boolean = false;
     eleveConnected: boolean = false;
     showLoginModal: boolean = false;
 
+    isEditModalOpen: boolean = false;
     errorServer: boolean = false;
+
+
     errorLogin: boolean = false;
 
     name: string = null;
 
     loginPassword: string = null;
     loginIsReady: boolean = false;
-    
-    subscriptionRoutingService: Subscription;
+    motdepasseConfirmation: string;
 
-    constructor(private router: Router, private apiService: APIService) { }
+    subscriptionRoutingService: Subscription;
+    isLoadingModal: boolean = false;
+    prenom: string;
+    nom: string;
+    motdepasse: string;
+    confirmationmotdepasse: string;
+
+    constructor(private router: Router, private formBuilder: FormBuilder,private apiService: APIService) { }
 
     ngOnInit()
     {
@@ -39,8 +53,17 @@ export class NavbarComponent implements OnInit, OnDestroy
             this.eleveConnected = RoutingService.eleveConnected;
             this.name = `${APIService.currentMaitresse.prenom} ${APIService.currentMaitresse.nom}`;
         });
+
     }
 
+
+
+
+
+
+
+
+    
     ngOnDestroy()
     {
         if (this.subscriptionRoutingService != null)
@@ -49,9 +72,71 @@ export class NavbarComponent implements OnInit, OnDestroy
 
     ShowProfile()
     {
-        console.log(APIService.token); ///////////////////////////////////////////////////////////////////////////////////////////////
-        ///////////////////////////////////////////////////////////////////////////////////////////////
+       // console.log(APIService.token);
+
+
+         
+               
+        this.prenom = APIService.currentMaitresse.prenom;
+                   
+        this.nom = APIService.currentMaitresse.nom;
+                
+
+
+
+            this.errorServer = false;
+            this.isEditModalOpen = true;
     }
+
+
+
+
+
+
+    SubmitEdit() {
+
+        var maitresse: Maitresse = new Maitresse();
+
+
+
+
+        maitresse.idMaitresse = APIService.currentMaitresse.idMaitresse;
+        maitresse.nom = this.nom;
+        maitresse.prenom = this.prenom;
+        maitresse.motdepasse = this.confirmationmotdepasse;
+
+        console.log(maitresse)
+
+
+        if (this.confirmationmotdepasse.length <= 8)
+            this.errorServer = false;
+        else       
+            this.apiService.AccountController_ProfilUpdate(maitresse).subscribe(
+                    (data: any) => {
+                        this.isLoadingModal = false;
+                        if (data.code == 200) {
+                           
+                            this.isEditModalOpen = false;
+                        }
+                        else
+                            this.errorServer = true;
+                    },
+                    () => {
+                        this.isLoadingModal = false;
+                        this.errorServer = true;
+                    });
+        
+    }
+
+
+
+
+
+
+
+
+
+
 
     GoToTableauDeBord()
     {
