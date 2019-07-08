f4c87ba81ad43e857dd672ce7d50813ee0af29b6
Jonathan Laberge <pm@jolab.me>
2019-07-08 18:37:18 -0400

    Profile QOS

--- Lexi/src/app/ui/dashboard/navbar/navbar.component.ts
+++ Lexi/src/app/ui/dashboard/navbar/navbar.component.ts
@@ -1,10 +1,8 @@
-import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { RoutingService } from 'src/app/service/routing.service';
 import { APIService } from 'src/app/service/api.service';
 import { Router } from '@angular/router';
-import { FormGroup, Validators, FormBuilder } from '@angular/forms';
-import { ClrForm, fade } from '@clr/angular';
 import { Maitresse } from 'src/app/model/maitresse';
 
 @Component(
@@ -12,47 +10,50 @@ import { Maitresse } from 'src/app/model/maitresse';
         selector: 'dashboard-navbar',
         templateUrl: './navbar.component.html'
     })
-export class NavbarComponent implements OnInit, OnDestroy {
-
-
+export class NavbarComponent implements OnInit, OnDestroy
+{
     isAdmin: boolean = false;
     eleveConnected: boolean = false;
+
     showLoginModal: boolean = false;
+    showProfileModal: boolean = false;
 
-    isEditModalOpen: boolean = false;
     errorServer: boolean = false;
-
-
-
     errorLogin: boolean = false;
 
     name: string = null;
 
     loginPassword: string = null;
     loginIsReady: boolean = false;
-    motdepasseConfirmation: string;
 
     subscriptionRoutingService: Subscription;
-    isLoadingModal: boolean = false;
+    subscriptionAccountControllerMode: Subscription;
+    subscriptionAccountControllerProfilUpdate: Subscription;
+
+    
     prenom: string;
     nom: string;
-
-
-    motdepasse2: string;
+    motdepasseConfirmation: string;
     motdepasse: string;
-    mismatch: boolean = true;
-    passwordLenght: boolean = false;
+    motdepasse2: string;
+
+    mismatch: boolean = false;
+    passwordLength: boolean = false;
     currentPasswordError: boolean = false;
 
+    isLoadingModal: boolean = false;
 
-    constructor(private router: Router, private formBuilder: FormBuilder, private apiService: APIService) { }
 
-    ngOnInit() {
+    constructor(private router: Router, private apiService: APIService) { }
+
+    ngOnInit()
+    {
         this.isAdmin = RoutingService.adminMode;
         this.eleveConnected = RoutingService.eleveConnected;
         this.name = `${APIService.currentMaitresse.prenom} ${APIService.currentMaitresse.nom}`;
 
-        this.subscriptionRoutingService = RoutingService.routeSubject.subscribe(() => {
+        this.subscriptionRoutingService = RoutingService.routeSubject.subscribe(() =>
+        {
             this.isAdmin = RoutingService.adminMode;
             this.eleveConnected = RoutingService.eleveConnected;
             this.name = `${APIService.currentMaitresse.prenom} ${APIService.currentMaitresse.nom}`;
@@ -60,180 +61,147 @@ export class NavbarComponent implements OnInit, OnDestroy {
 
     }
 
+    ngOnDestroy()
+    {
+        if (this.subscriptionRoutingService != null)
+            this.subscriptionRoutingService.unsubscribe();
 
+        if (this.subscriptionRoutingService != null)
+            this.subscriptionRoutingService.unsubscribe();
 
-
-
-
-
-
-
-    ngOnDestroy() {
         if (this.subscriptionRoutingService != null)
             this.subscriptionRoutingService.unsubscribe();
     }
 
-    ShowProfile() {
-        // console.log(APIService.token);
-
-
-
-
+    ShowProfile()
+    {
         this.prenom = APIService.currentMaitresse.prenom;
-
         this.nom = APIService.currentMaitresse.nom;
 
-
-
-
         this.errorServer = false;
-        this.isEditModalOpen = true;
+        this.showProfileModal = true;
     }
 
+    SubmitAdminMode()
+    {
+        var args = { motdepasse: this.loginPassword };
 
-    closeEdit() {
-
-
-        this.motdepasseConfirmation = "";
-        this.motdepasse = "";
-        this.motdepasse2 = "";
-        this.isEditModalOpen = false;
-
-
-    }
-
-
-
-    SubmitEdit() {
-
-        var maitresse: Maitresse = new Maitresse();
+        this.subscriptionAccountControllerMode =
+            this.apiService.AccountController_Mode(args).subscribe(
+                (data: any) =>
+                {
+                    if (data.token != null)
+                    {
+                        APIService.token = data.token;
+                        localStorage.setItem('token', JSON.stringify(APIService.token));
+                        RoutingService.adminMode = true;
+                        RoutingService.eleveConnected = false;
+                        RoutingService.SetRouteToAdmin();
 
 
+                        this.errorServer = false;
+                        this.loginIsReady = true;
+                        this.router.navigate(['/tableaudebord']);
+                    }
+                    else
+                    {
+                        this.errorServer = true;
+                        this.loginIsReady = true;
+                    }
 
-
-        maitresse.idMaitresse = APIService.currentMaitresse.idMaitresse;
-        maitresse.nom = this.nom;
-        maitresse.prenom = this.prenom;
-        maitresse.motdepasseConfirmation = this.motdepasseConfirmation;
-        maitresse.motdepasse = this.motdepasse;
-        console.log(maitresse)
-
-
-
-        this.apiService.AccountController_ProfilUpdate(maitresse).subscribe(
-            (data: any) => {
-                this.isLoadingModal = false;
-                if (data.code == 200) {
-                    this.closeEdit();
-                }
-                else
-                    this.errorServer = true;
-            },
-            (error) => {
-                this.isLoadingModal = false;
-                switch (error.status) {
-
-                    case 401: this.currentPasswordError = true;
-                        break;
-                    default:
-
+                },
+                error =>
+                {
+                    if (error.status == 401)
+                        this.errorLogin = true;
+                    else
                         this.errorServer = true;
 
-                        break;
-
-                }
-            });
-
+                    this.loginIsReady = true;
+                });
     }
 
-
-
-
-
-
-
-
-
-
-
-    GoToTableauDeBord() {
+    GoToTableauDeBord()
+    {
         if (APIService.IsTokenInAdminMode())
             this.router.navigate(['/tableaudebord']);
         else
             this.showLoginModal = true;
     }
 
-    SubmitAdminMode() {
-        var args = { motdepasse: this.loginPassword };
-
-        this.apiService.AccountController_Mode(args).subscribe(
-            (data: any) => {
-                if (data.token != null) {
-                    APIService.token = data.token;
-                    localStorage.setItem('token', JSON.stringify(APIService.token));
-                    RoutingService.adminMode = true;
-                    RoutingService.eleveConnected = false;
-                    RoutingService.SetRouteToAdmin();
-
-
-                    this.errorServer = false;
-                    this.loginIsReady = true;
-                    this.router.navigate(['/tableaudebord']);
-                }
-                else {
-                    this.errorServer = true;
-                    this.loginIsReady = true;
-                }
-
-            },
-            error => {
-                if (error.status == 401)
-                    this.errorLogin = true;
-                else
-                    this.errorServer = true;
-
-                this.loginIsReady = true;
-            });
-    }
-
-    GoToElevePortail() {
+    GoToElevePortail()
+    {
         this.router.navigate(['/eleve']);
     }
 
-    Logout() {
+    Logout()
+    {
         RoutingService.Logout();
 
         this.router.navigate(['/']);
     }
 
+    CloseProfile()
+    {
+        this.motdepasseConfirmation = "";
+        this.motdepasse = "";
+        this.motdepasse2 = "";
+        this.showProfileModal = false;
+    }
+    
+    SubmitEditProfile()
+    {
+        var maitresse: Maitresse = new Maitresse();
 
+        maitresse.idMaitresse = APIService.currentMaitresse.idMaitresse;
+        maitresse.nom = this.nom;
+        maitresse.prenom = this.prenom;
+        maitresse.motdepasseConfirmation = this.motdepasseConfirmation;
+        maitresse.motdepasse = this.motdepasse;
 
+        this.subscriptionAccountControllerProfilUpdate =
+            this.apiService.AccountController_ProfilUpdate(maitresse).subscribe(
+                (data: any) =>
+                {
+                    this.isLoadingModal = false;
+                    if (data.code == 200)
+                    {
+                        this.CloseProfile();
+                    }
+                    else
+                        this.errorServer = true;
+                },
+                (error) =>
+                {
+                    this.isLoadingModal = false;
+                    switch (error.status)
+                    {
+                        case 401:
+                            this.currentPasswordError = true;
+                            break;
 
-
-
-
-
-
-
-
-
-    confirmPassword() {
-
-        if (this.motdepasseConfirmation.length <= 8)
-            this.passwordLenght = true;
-        else
-            this.passwordLenght = false;
-
-        if (this.motdepasse2 != this.motdepasse) {
-
-            console.log(" error ");
-            this.mismatch = true;
-
-        } else {
-            this.mismatch = false;
-        }
-
-
+                        default:
+                            this.errorServer = true;
+                            break;
+                    }
+                });
     }
 
+    CheckPassword(): boolean
+    {
+        if (this.motdepasse.length < 8)
+            this.passwordLength = true;
+        else
+            this.passwordLength = false;
+
+        if (this.motdepasse2 != this.motdepasse)
+            this.mismatch = true;
+        else
+            this.mismatch = false;
+
+        if (this.mismatch || this.passwordLength)
+            return true;
+
+        return false;
+    }
 }
